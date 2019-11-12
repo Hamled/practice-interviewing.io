6ba4723f6fe3cc61abdb84de1428d5e64513ee3a
Charles Ellis <hamled@hamled.name>
2019-11-11 22:38:44 -0800

    Maximum subarray - attempt 1

---
+++ max_subarray.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def max_subarray(ns):
+    sums = []
+    current = 0  # Current subarray sum
+    cost = 0  # Cost to merge w/ last subarray sum
+    for n in ns:
+        if n < 0:  # end of a positive subarray
+            cost -= n
+            continue
+        if current - cost < 0:  # cannot merge
+            if current > 0:
+                sums.append(current)
+            current = n
+        else:  # merge positive subarrays
+            current += n - cost
+
+        cost = 0
+
+    if current > 0:
+        sums.append(current)
+
+    # No positive subarrays, so the max is
+    # the single entry with highest value
+    if len(sums) == 0:
+        return max(ns)
+
+    return max(sums)
